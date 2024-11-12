05cc7c84f547d9aa3960ecdb07a32f479bf3eeee
023javi <javier023javi@gmail.com>
2024-11-13 00:48:32 +0100

    feat: finish

--- main.c
+++ main.c
@@ -17,10 +17,17 @@ void buscar_substring(char _string_DNIs[N][9+1]) {
     printf("\nSubstring a buscar ? ");
     char substring[9+1];
     scanf("%s", substring);
+
+    int found = 0;
     for (int i = 0; i < N; i++) {
         if(strstr(_string_DNIs[i], substring) != NULL) {
             printf("> %s", _string_DNIs[i]);
+            found = 1;
         }
+    }
+
+    if (!found) {
+        printf("Not found");
     }
 }
 
@@ -28,12 +35,20 @@ void buscar_letra(char _string_DNIs[N][9+1]) {
     printf("\nLetra a buscar? ");
     char letra;
     letra = getch();
+    while(getchar() != '\n');
 
+    int found = 0;
     for (int i = 0; i < N; i++) {
         if (_string_DNIs[i][8] == letra) {
             printf("> %s ", _string_DNIs[i]);
+            found = 1;
         }
     }
+
+    if (!found) {
+        printf("Not found");
+    }
+
 }
 
 void print_strings_DNIs(char string_DNIs[N][9+1]) {
@@ -51,28 +66,23 @@ void rand_strs_DNI(char string_DNIs[N][9+1]) {
 }
 
 void print_menu() {
-
     char string_DNIs[N][9+1];
     rand_strs_DNI(string_DNIs);
-
-    printf("\n1 buscar letra\n");
-    printf("2 buscar substring\n");
-    printf("0 END\n");
-
     char menu;
-    menu = getch();
 
     do {
+        printf("\n1 buscar letra\n");
+        printf("2 buscar substring\n");
+        printf("0 END\n");
+        menu = getch();
+        while(getchar() != '\n');
         if (menu == '1') {
             buscar_letra(string_DNIs);
         }else if (menu == '2') {
             buscar_substring(string_DNIs);
         }
-    }while(menu == '0');
+    }while(menu != '0');
 }
-
-
-
 
 void rand_str_DNI(char str_DNI[9+1]) {
     int digit = 0, dni_number = 0, rest = 0;
@@ -90,7 +100,6 @@ void rand_str_DNI(char str_DNI[9+1]) {
 int main(void)
 {
     srand(time(NULL));
-
     print_menu();
     return 0;
 }
