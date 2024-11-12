5e9be72997e0586f2a1507caf3dc1c4f6041febc
023javi <javier023javi@gmail.com>
2024-11-12 23:52:00 +0100

    feat: add print_menu method

--- main.c
+++ main.c
@@ -12,6 +12,30 @@ void print_strings_DNIs(char string_DNIs[N][9+1]);
 void rand_strings_DNIs(char string_DNIs[N][9+1]);
 void rand_str_DNI(char str_DNI[9+1]);
 
+void buscar_substring(char _string_DNIs[N][9+1]) {
+
+}
+
+void buscar_letra(char _string_DNIs[N][9+1]) {
+
+}
+
+void print_menu() {
+    printf("\n1 buscar letra\n");
+    printf("2 buscar substring\n");
+    printf("0 END\n");
+
+    char menu, string_DNIs[N][9+1];
+    menu = getchar();
+
+    do {
+        if (menu == '1') {
+            buscar_letra(string_DNIs);
+        }else if (menu == '2') {
+            buscar_substring(string_DNIs);
+        }
+    }while(menu == '0');
+}
 
 void print_strings_DNIs(char string_DNIs[N][9+1]) {
     for (int i = 0; i < N; i++) {
@@ -45,5 +69,6 @@ int main(void)
     srand(time(NULL));
     char string_DNIs[N][9+1];
     rand_strs_DNI(string_DNIs);
+    print_menu();
     return 0;
 }
